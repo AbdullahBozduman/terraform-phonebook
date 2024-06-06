e3f42d4dfeb81c6c6814f7b1c62da7541013a12e
Abdullah <thecryptonot@gmail.com>
2024-06-06 12:17:52 +0300

    first-commit

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "AbdullahBozduman"
+}
+
+variable "git-token" {
+    default = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+}
+
+variable "key-name" {
+  default = "firstkey"
+}
+
+variable "hosted-zone" {
+  default = "clarkstone.click"
+}
